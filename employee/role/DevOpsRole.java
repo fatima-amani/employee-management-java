dfa796cc787bcbdf8fc5871f0b7ee13d16c25bb3
fatima <fatima.amani65@gmail.com>
2025-08-25 19:07:01 +0530

    fix roles enum

--- employee/role/DevOpsRole.java
+++ employee/role/DevOpsRole.java
@@ -1,6 +1,16 @@
 package employee.role;
 
 public enum DevOpsRole {
-    SENIOR_DEVOPS_ENGINEER,
-    JUNIOR_DEVOPS_ENGINEER
+    SENIOR_DEVOPS_ENGINEER("Senior DevOps Engineer"),
+    JUNIOR_DEVOPS_ENGINEER("Junior DevOps Engineer");
+
+    private final String role;
+
+    DevOpsRole(String role) {
+        this.role = role;
+    }
+
+    public String getrole() {
+        return role;
+    }
 }
--- employee/role/DeveloperRole.java
+++ employee/role/DeveloperRole.java
@@ -1,9 +1,17 @@
 package employee.role;
 
 public enum DeveloperRole {
+    FRONTEND("Frontend Developer"),
+    BACKEND("Backend Developer"),
+    FULLSTACK("Full Stack Developer"),
 
-        FRONTEND,
-        BACKEND,
-        FULLSTACK
+    private final String role;
 
+    DeveloperRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
